4ebef6758a3c7f1e3e52b509e06e0cd63b8a62b1
chykea <78532156@qq.com>
2023-08-10 18:24:03 +0800

    fix

--- server/app.js
+++ server/app.js
@@ -1,20 +1,9 @@
 import Koa from 'koa'
 import { koaBody } from 'koa-body'
 import base from './router/index.js'
-import Router from 'koa-router'
 
 const app = new Koa()
 
-// import Router from 'koa-router'
-// const router = new Router()
-
-
-// router.get('/', (ctx, next) => {
-//     ctx.body = 'hello koa'
-// })
-// router.get('/test', (ctx, next) => {
-//     ctx.body = 'test'
-// })
 
 app.use(koaBody())
 
